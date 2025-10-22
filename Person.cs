039263f804bed66ec242a0cc4292d9f407ba6734
VAIII <danilkasuralev@gmail.com>
2025-10-22 10:31:10 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic_work_9
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Ошибка");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0 && value > 120)
+                {
+                    Console.WriteLine("Ошибка");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void SayHello()
+        {
+            Console.WriteLine($"Привет, я {name}, мне {age} лет»");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            circle.GetArea();
+            try
+            {
+                // некорректный радиус
+                circle.Radius = -2;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
