d68ad1ddb91f10ae87c7e1e88226352c94f21177
VAIII <danilkasuralev@gmail.com>
2025-10-22 10:37:03 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -4,23 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Circle
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Task1Person
+     Person person = new Person("Егорчик", 19);
+     person.SayHello();
+        }
+    }
+}
+namespace Task2Phone
+            Phone phone = new Phone("Samsung", 82);
+            phone.UsePhone();
+            phone.UsePhone();
+        }
+    }
+}
+namespace Task3BackAccount
+                BankAccount account = new BankAccount("Егор", 500); 
+            account.Deposit(500);
+            account.Withdraw(500);
+            account.Withdraw(1000);
+        }
+    }
+}
+namespace Task4Circle
             Circle circle = new Circle(5);
-            circle.GetArea();
-            try
-            {
-                // некорректный радиус
-                circle.Radius = -2;
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+        circle.GetArea();
+        try
+        {
+            // некорректный радиус
+            circle.Radius = -2;
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+namespace Task5Pet
+            Pet cat = new Pet("Барсик",35);
+            cat.Play();    
+            cat.Rest();    
         }
     }
 }
